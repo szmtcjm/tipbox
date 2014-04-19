67b8cd974cc0dbca0b6c0bfd8cddc665f5f51ef7
chenjiangming <szmtcjm@126.com>
2014-04-19 21:40:24 +0800

    complete-1

--- app/js/angular-tipbox.js
+++ app/js/angular-tipbox.js
@@ -1,12 +1,15 @@
 (function(window, angular, undefined) {
 	angular.module('tipbox', []).
 		directive('cTipbox', function() {
-			var boxTemplater = '<span class="symbol1">&#9670;</em><span class="symbol2">&#9670;</span> '
+			var boxTemplater = '<span style="display:block;width:30px;height:16px;font-size:30px;overflow:hidden;position:relative;margin-left:10px;margin-top:-15px;color:black">&#9670;</span> \
+			                    <span style="display:block;width:30px;height:16px;font-size:30px;overflow:hidden;position:relative;margin-left:10px;margin-top:-14px;color:white">&#9670;</span> \
+			                    <div ng-transclude></div>';
 			return {
 				restrict: 'E',
 				transclude: true,
 				template: boxTemplater,
 				link: function(scope, element, attrs) {
+					console.log(element)
 					element.css({
 						position: 'absolute',
 						display: 'block',
@@ -14,26 +17,14 @@
 						height: '200px',
 						top: '20px',
 						left: '200px',
-						border: '1px solid black'
+						border: '1px solid black',
+						'border-radius': '10px',
+						'-moz-border-radius':'25px',
+						'box-shadow': '2px 2px 3px #aaaaaa',
+						'-moz-box-shadow': '2px 2px 3px #aaaaaa'
 					});
 
-					element.children('.symbol1 .symbol2').css({
-						display: 'block',
-						width: '30px',
-						height: '16px',
-						'font-size': '30px',
-						overflow: 'hidden',
-						'_position': 'relative',
-						'margin-left': '10px'
-					});
-					element.children('.symbol1').css({
-						'margin-top': '-16px',
-						color: 'black',
-					});
-					element.children('symbol2').css({
-						'margin-top': '-14px',
-						color: 'white'
-					});
+					//element.find('symbol1')
 				}
 			};
 		}).
